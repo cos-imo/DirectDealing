60bcb3a606928e131d72b4ca6c240af4d0bf0675
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-01-08 20:28:40 +0100

    [feature] Utilisation d'icônes sur les menus

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module eu.telecomnancy.labfx {
     requires javafx.fxml;
 
     exports eu.telecomnancy.labfx.Controller;
-    
+
     opens eu.telecomnancy.labfx to javafx.fxml;
     exports eu.telecomnancy.labfx;
 }
