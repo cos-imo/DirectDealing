b747f87a61e474ffe0ec5fb537b1926b2c8f0654
DECAVE Gabriel <gabriel.decave@telecomnancy.eu>
2024-01-08 23:54:26 +0100

    jsp dsl cosimo

--- src/main/java/eu/telecomnancy/labfx/Controller/CreationController.java
+++ src/main/java/eu/telecomnancy/labfx/Controller/CreationController.java
@@ -1,5 +1,12 @@
 package eu.telecomnancy.labfx.Controller;
 
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import eu.telecomnancy.labfx.Connect;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -27,7 +34,7 @@ public class CreationController {
     private Label errorLabel;
 
     @FXML
-    private void setCreateAccountBtn(){
+    private void setCreateAccountBtn() throws SQLException{
         String prenom = prenomField.getText();
         String nom = nomField.getText();
         String email = emailField.getText();
@@ -35,8 +42,35 @@ public class CreationController {
         String password = passwordField.getText();
         String passwordConfirm = passwordConfirmField.getText();
         if (infosValid(prenom, nom, email, emailConfirm, password, passwordConfirm)){
-            //TODO : create account
+            if (createAccount(prenom, nom, email, password)){
+                System.out.println("Compte créé");
+            }
+            else{
+                System.out.println("Erreur lors de la création du compte");
+            }
         }
+    }
+
+    private boolean createAccount(String prenom, String nom, String email, String password) throws SQLException{
+        Connect connect = new Connect();
+        Connection connection = connect.getConnection(); 
+        String sql = "INSERT INTO User (First_Name, Last_Name, Mail, Password) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, prenom);
+            preparedStatement.setString(2, nom);
+            preparedStatement.setString(3, email);
+            preparedStatement.setString(4, password);
+
+            // Requête d'insertion
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            // Retourner vrai si une ligne a été insérée, faux sinon
+            return rowsAffected > 0;
+            }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
     
     private boolean infosValid(String prenom, String nom, String email, String emailConfirm, String password, String passwordConfirm){
