d0ae845925b922378836bc0a37723979bb3e78f0
DECAVE Gabriel <gabriel.decave@telecomnancy.eu>
2024-01-09 21:24:31 +0100

    [chore] ajout vue pref aux users

---
+++ src/main/java/eu/telecomnancy/labfx/Recherche.java
@@ -0,0 +1,19 @@
+package eu.telecomnancy.labfx;
+
+import java.awt.Color;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class Recherche {
+    
+}
