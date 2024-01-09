168c9e4f74fca4a66214829bbc6d9f4121f96a38
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-01-09 19:05:05 +0100

    [feature] Le résultat de la recherche cherche désormais dans la base de donnée

---
+++ src/main/java/eu/telecomnancy/labfx/Controller/ListObjectController.java
@@ -0,0 +1,38 @@
+package eu.telecomnancy.labfx.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import eu.telecomnancy.labfx.Connect;
+import java.io.IOException;
+import java.sql.*;
+
+public class ListObjectController{
+
+    @FXML
+    Label label_nom;
+
+    @FXML
+    Label label_type;
+
+    @FXML
+    Label label_nomPreteur;
+
+    @FXML
+    Label prix_florains;
+
+    public void setElementData(String annonceName, String type, String preteur, String cout){
+        label_nom.setText(annonceName);
+        label_type.setText(type);
+        label_nomPreteur.setText(preteur);
+        prix_florains.setText(cout);
+    }
+}
