f5b84ef7dad734e2207a6361ddad2f41a2ffe931
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-01-09 10:33:08 +0100

    [feature] Le bouton Accueil permet désormais de retourner à l'accueil

--- src/main/java/eu/telecomnancy/labfx/Controller/HeaderController.java
+++ src/main/java/eu/telecomnancy/labfx/Controller/HeaderController.java
@@ -9,6 +9,15 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.concurrent.Worker.State;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HeaderController extends HBox {
     @FXML
@@ -34,6 +43,30 @@ public class HeaderController extends HBox {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    public void retourAccueil(ActionEvent event){
+            BorderPane root = new BorderPane();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/eu/telecomnancy/labfx/fxml/Accueil.fxml"));
+
+            Scene scene = new Scene(root, 1080, 720);
+
+            // HeaderController header = new HeaderController();
+            // root.setTop(header);
+            try {
+                root.setCenter(loader.load());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Node source = (Node) event.getSource();
+            Stage primaryStage = (Stage) source.getScene().getWindow();
+
+            primaryStage.setTitle("TelecomNancy DirectDealing");
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
     }
     
 }
