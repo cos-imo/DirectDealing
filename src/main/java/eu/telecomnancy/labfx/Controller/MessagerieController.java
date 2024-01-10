44042816aaf678919f43f2dd0d6d8c402ec49b5f
DECAVE Gabriel <gabriel.decave@telecomnancy.eu>
2024-01-10 14:04:36 +0100

    [Feat] Chgmt requête messagerie

--- src/main/java/eu/telecomnancy/labfx/Controller/MessagerieController.java
+++ src/main/java/eu/telecomnancy/labfx/Controller/MessagerieController.java
@@ -12,6 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import eu.telecomnancy.labfx.Connect;
+import eu.telecomnancy.labfx.Session;
 
 public class MessagerieController {
 
@@ -27,7 +28,7 @@ public class MessagerieController {
 
     private void addElementToMessageList(String sender, String contenu) throws SQLException {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/eu/telecomnancy/labfx/fxml/Conversation.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/eu/telecomnancy/labfx/fxml/BandeauConversation.fxml"));
             Node content = loader.load();
 
             ConversationController objectController = loader.getController();
@@ -44,13 +45,18 @@ public class MessagerieController {
 
     private void getMessages() throws SQLException {
         Connect connect = new Connect();
+        int user_id = Session.getInstance().getCurrentUser().getId();
         try (Connection connection = connect.getConnection()){
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Message WHERE ;");
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT DISTINCT e.Event_id, m.Contenu, CASE WHEN m.Sender_id < m.Receiver_id THEN m.Sender_id ELSE m.Receiver_id END AS user1_id, CASE WHEN m.Sender_id < m.Receiver_id THEN m.Receiver_id ELSE m.Sender_id END AS user2_id FROM Message m JOIN Event e ON m.Event_lie_id = e.Event_id WHERE m.Sender_id = ? OR m.Receiver_id = ?;"
+                );
+            preparedStatement.setInt(1, user_id);
+            preparedStatement.setInt(2, user_id);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                String sender = resultSet.getString(3);
-                String message = resultSet.getString(5);
+                String sender = resultSet.getString("user1_id");
+                String message = resultSet.getString("Contenu");
                 addElementToMessageList(sender, message);
             }
             resultSet.close();
