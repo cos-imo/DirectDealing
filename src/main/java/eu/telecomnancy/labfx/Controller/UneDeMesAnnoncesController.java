efe4d920c8057eee37a653e6af9f9d5423c1bb95
DECAVE Gabriel <gabriel.decave@telecomnancy.eu>
2024-01-12 17:28:18 +0100

    [Fix] image apparait désormais dans mes annonces

--- src/main/java/eu/telecomnancy/labfx/Controller/UneDeMesAnnoncesController.java
+++ src/main/java/eu/telecomnancy/labfx/Controller/UneDeMesAnnoncesController.java
@@ -35,33 +35,30 @@ public class UneDeMesAnnoncesController{
 
 
     public void setElementData(String annonceName, Boolean type, int Prix, Image image, DateTime dateDebut, DateTime dateFin, Recurrence reccurence){
-        System.out.println(annonceName);
         //____Image____
-        if (image_annonce!=null){
+        if (image!=null){
             image_annonce.setImage(image);
         }
-        System.out.println(("1"));
 
         //____Nom____
         label_nom.setText(annonceName);
-        System.out.println(("2"));
+
         //____Type____
         label_type.setText(type ? "Objet" : "Service");
-        System.out.println(("3"));
+
 
         //____Date____
         DateTime dateDebutAnnonce = dateDebut;
         DateTime dateFinAnnonce = dateFin;
         label_date.setText("Du " + dateDebutAnnonce.getYear() + "-" + dateDebutAnnonce.getMonthOfYear() + "-" + dateDebutAnnonce.getDayOfMonth() + " " + dateDebutAnnonce.getHourOfDay() + ":" + dateDebutAnnonce.getMinuteOfHour() + ":" + dateDebutAnnonce.getSecondOfMinute() + " au " + dateFinAnnonce.getYear() + "-" + dateFinAnnonce.getMonthOfYear() + "-" + dateFinAnnonce.getDayOfMonth() + " " + dateFinAnnonce.getHourOfDay() + ":" + dateFinAnnonce.getMinuteOfHour() + ":" + dateFinAnnonce.getSecondOfMinute());
-        System.out.println(("4"));
+
 
         //____Prix_____
         prix_florains.setText(Prix+"");
-        System.out.println(("5"));
+
 
         //____Recurrence_____
         label_Recurence.setText(reccurence.toString());   
-        System.out.println(("on est ici"));
     }
 
 }
